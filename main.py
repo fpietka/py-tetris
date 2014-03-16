f5d0a67b7258b7039869503c82a5d26102325a23
François Pietka <francois@pietka.fr>
2014-03-16 14:34:36 +0100

    create all tetriminos

--- main.py
+++ main.py
@@ -31,6 +31,57 @@ class Tetrimino():
         self.color = color
         return self
 
+tetriminos = (
+    Tetrimino('O').setBlocks((
+            pygame.Rect(0, 0, 10, 10),
+            pygame.Rect(12, 0, 10, 10),
+            pygame.Rect(12, 12, 10, 10),
+            pygame.Rect(0, 12, 10, 10)
+    )).setColor(yellow),
+
+    Tetrimino('I').setBlocks((
+            pygame.Rect(0, 0, 10, 10),
+            pygame.Rect(12, 0, 10, 10),
+            pygame.Rect(24, 0, 10, 10),
+            pygame.Rect(36, 0, 10, 10)
+    )).setColor(cyan),
+
+    Tetrimino('J').setBlocks((
+            pygame.Rect(0, 0, 10, 10),
+            pygame.Rect(0, 12, 10, 10),
+            pygame.Rect(12, 12, 10, 10),
+            pygame.Rect(24, 12, 10, 10)
+    )).setColor(blue),
+
+    Tetrimino('L').setBlocks((
+            pygame.Rect(0, 12, 10, 10),
+            pygame.Rect(12, 12, 10, 10),
+            pygame.Rect(24, 12, 10, 10),
+            pygame.Rect(24, 0, 10, 10)
+    )).setColor(orange),
+
+    Tetrimino('S').setBlocks((
+            pygame.Rect(0, 12, 10, 10),
+            pygame.Rect(12, 0, 10, 10),
+            pygame.Rect(12, 12, 10, 10),
+            pygame.Rect(24, 0, 10, 10)
+    )).setColor(green),
+
+    Tetrimino('T').setBlocks((
+            pygame.Rect(0, 12, 10, 10),
+            pygame.Rect(12, 0, 10, 10),
+            pygame.Rect(12, 12, 10, 10),
+            pygame.Rect(24, 12, 10, 10)
+    )).setColor(purple),
+
+    Tetrimino('Z').setBlocks((
+            pygame.Rect(0, 0, 10, 10),
+            pygame.Rect(12, 0, 10, 10),
+            pygame.Rect(12, 12, 10, 10),
+            pygame.Rect(24, 12, 10, 10)
+    )).setColor(red),
+)
+
 running = True
 while running:
     for event in pygame.event.get():
