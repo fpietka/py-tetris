c8b3e930b7ef855ddeec909f4979691b1543116c
François Pietka <francois@pietka.fr>
2014-03-16 14:33:00 +0100

    add some base colors

--- main.py
+++ main.py
@@ -9,6 +9,14 @@ mode = modes[-1]
 # Running in smallest mode
 screen = pygame.display.set_mode(mode)
 
+yellow = (255, 255, 0)
+cyan = (0, 255, 255)
+red = (255, 0, 0)
+blue = (0, 0, 255)
+green = (0, 255, 0)
+purple = (255, 0, 255)
+orange = (255, 255, 0)
+
 running = True
 while running:
     for event in pygame.event.get():
