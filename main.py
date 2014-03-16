34f4cdb59ba95a9e7e0fab14ae02ddfe1f736965
François Pietka <francois@pietka.fr>
2014-03-16 14:32:17 +0100

    add main loop

--- main.py
+++ main.py
@@ -8,3 +8,14 @@ modes = pygame.display.list_modes()
 mode = modes[-1]
 # Running in smallest mode
 screen = pygame.display.set_mode(mode)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key in (pygame.K_ESCAPE, pygame.K_q):
+                running = False
+
+print("Exiting game")
