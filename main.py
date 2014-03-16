9a6957022999a4ae5fa6dc5e3e278e2e6c2a6912
François Pietka <francois@pietka.fr>
2014-03-16 14:33:56 +0100

    add Tetrimino's class

--- main.py
+++ main.py
@@ -17,6 +17,20 @@ green = (0, 255, 0)
 purple = (255, 0, 255)
 orange = (255, 255, 0)
 
+class Tetrimino():
+    def __init__(self, name):
+        self.name = name
+        self.blocks = list()
+        self.color = (255, 255, 255)
+
+    def setBlocks(self, blocks):
+        self.blocks = blocks
+        return self
+
+    def setColor(self, color):
+        self.color = color
+        return self
+
 running = True
 while running:
     for event in pygame.event.get():
