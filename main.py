920bdd3561ed1e00cfa72e702d756c87a7074897
François Pietka <francois@pietka.fr>
2016-01-21 18:46:13 +0100

    Remove dead code

--- main.py
+++ main.py
@@ -8,23 +8,18 @@ pygame.init()
 pygame.mouse.set_visible(False)
 
 # Init config
+"""
+with open('infile.txt') as f:
+    {int(k): v for line in f for (k, v) in (line.strip().split(None, 1),)}
+    """
 config = {}
 execfile("controls.conf", config)
+print(config)
 
 modes = pygame.display.list_modes()
 mode = modes[-1]
 # Running in smallest mode
 screen = pygame.display.set_mode(mode)
-
-D_UP = 0
-D_RIGHT = 1
-D_DOWN = 2
-D_LEFT = 3
-directions = (D_UP, D_RIGHT, D_DOWN, D_LEFT)
-
-M_CW = 0
-M_CCW = 1
-motions = (M_CW, M_CCW)
 
 game = Game(screen, config)
 while game.running:
